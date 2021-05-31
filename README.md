1cc09e1f67f2c39664e8b2aaa9bca8d060f2385b
ArnaudAbreu <arnaud.abreu.p@gmail.com>
2021-05-31 13:34:40 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+<div align="center">
+
+# PathoBias
+
+
+**Bias handling for bio-medical cohorts.**
+
+---
+
+## PathoBias aims to ease bias detection and mitigation for bio-medical datasets.
+It produces html/pdf dashboards to give an insight on potential bias sources. It also provides some tools to mitigate bias and study ML solutions in a FAIR environment.
+
+---
+
+## Basic Usage
+
+### Step 0: Install
+
+Simple installation from local package
+```bash
+pip install path/to/PathoBias
+```
+
+---
+
+## License
+
+Please observe the GNU GPL 3.0 license that is listed in this repository.
---
+++ setup.py
@@ -0,0 +1,25 @@
+"""Setup script to install pathobias."""
+from setuptools import setup, find_packages
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+    name="pathobias",
+    version="0.0.0",
+    description="procedures for bias detection and mitigation",
+    author="Arnaud Abreu",
+    author_email="arnaud.abreu.p@gmail.com",
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=[
+        "numpy",
+        "tqdm"
+    ],
+    include_package_data=True,
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+)
